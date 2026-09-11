4bb91eb753a2bf4374b76765dbc473560540fdea
juninhoatituderezende-cmd <juninhoatituderezende@gmail.com>
2026-09-11 19:21:37 -0300

    fix: spinner class typo

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,7 @@ const App = () => (
   <ErrorBoundary>
     <Suspense fallback={
       <div className="flex items-center justify-center min-h-screen">
-        <div className="animate-spin rounded-rounded-full h-32 w-32 border-b-2 border-primary"></div>
+        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-primary"></div>
       </div>
     }>
       <BrowserRouter>
